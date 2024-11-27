ee755e2a46aeca0c583bf70f98f6556f9243d841
Muhammad Noman <nomanaligr4@gmail.com>
2024-11-27 11:31:58 +0500

    Deploy updated version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/weather-dashboard/', // Important: ensure this matches your GitHub repo name
+});
